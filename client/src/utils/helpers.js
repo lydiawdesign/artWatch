31b77760967dbe55302a21a996f65fcb16a855b7
haleywitherell <hwitherell22@gmail.com>
2022-01-15 13:11:07 -0500

    updated helpers to match changes for componets

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,10 +1,3 @@
-export function pluralize(name, count) {
-  if (count === 1) {
-    return name;
-  }
-  return name + 's';
-}
-
 export function idbPromise(storeName, method, object) {
   return new Promise((resolve, reject) => {
     const request = window.indexedDB.open('shop-shop', 1);
