a394f51fa29f088720b6956d04555d52c1848413
haleywitherell <hwitherell22@gmail.com>
2022-01-15 21:13:54 -0500

    navbar styling

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
+
 const Navbar = () => {
     return (
         <div>
-            <nav>
-                test
-            </nav>
+                <nav className="nav navbar navbar-expand-sm navbar-color">
+                    <h1 className="text-light">artWatch</h1>
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a href="/" className="nav-link text-light">Login/Signup</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/" className="nav-link text-light">WatchList</a>
+                            </li>
+                        </ul>
+                </nav>
         </div>
     )
 }
