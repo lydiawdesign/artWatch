9295cd234799c8ff31ff3c1042193414403ec540
lydiawdesign <lydia.williamson.OSU@gmail.com>
2022-01-16 10:23:53 -0500

    group got the nav working

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <div>
              <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-                <div className="navbar-brand mx-5" id="artWatchStyle" href="#about">art<a>Watch</a></div>
+                <a className="navbar-brand mx-5" id="artWatchStyle" href="/">art<a>Watch</a></a>
 
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -15,15 +15,15 @@ const Navbar = () => {
                     <ul className="navbar-nav ms-auto mx-5">
 
                         <li className="nav-item active">
-                            <a className="nav-link" href="#login">Login <span className="sr-only"></span></a>
+                            <a className="nav-link" href="Login">Login <span className="sr-only"></span></a>
                         </li>
 
                         <li className="nav-item">
-                            <a className="nav-link" href="#signup">Register</a>
+                            <a className="nav-link" href="Signup">Sign Up</a>
                         </li>
 
                         <li className="nav-item">
-                            <a className="nav-link" href="#watchlist">Watchlist</a>
+                            <a className="nav-link" href="Watchlist">Watchlist</a>
                         </li>
                     </ul>
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,7 +8,6 @@ const Home = () => {
     <div className="container">
       <ProductList/>
       {/* <CategoryMenu /> */}
-      {/* <ProductList /> */}
       {/* <Watchlist /> */}
     </div>
   );
