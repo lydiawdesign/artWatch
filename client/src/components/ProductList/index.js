d89ed104fbfda63192305402c1e75f1b9e69f4f9
MJ <mdjenkins655@gmail.com>
2022-01-17 21:38:00 -0500

    fixed front end

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -7,23 +7,25 @@ import { QUERY_ALL_PRODUCTS } from '../../utils/queries';
 
 
 export const ProductList = () => {
-    const {loading, data} = useQuery(QUERY_ALL_PRODUCTS);
-
+    const {loading, data, error} = useQuery(QUERY_ALL_PRODUCTS);
+    console.log("***",data)
+    console.log(JSON.stringify(error,null,2))
     const products = data?.products || []; 
-    console.log(products);
+    console.log("---",products);
     return (
+      
         <div className="container p-5">
             <h1 className='text-center pt-5'>View our Gallery:</h1>
 
            <div className="card-group">
 
                {products.map(product => (
-                   <div className="card g-3 p-2">
-                   <img src={Azaman1} className="card-img-top" alt="Azaman"/>
+                   <div className="card col-3" key={product._id}>
+                   <img src={`/images/${product.image}`} className="card-img-top" alt="Artwork"/>
                    <div className="card-body card text-center">
-                       <h5 className="card-title">Azaman 1</h5>
-                       <p className="card-text">this is a short description for the art. </p>
-                       <p className="card-text"> start bid is $300</p>
+                       <h5 className="card-title">{product.title}</h5>
+                       <p className="card-text">{product.description} </p>
+                       <p className="card-text"> {product.startBid}</p>
                        <a href="gallery" className="btn btn-outline-dark m-2 buttonStyle" target='_blank' rel="noreferrer">View Product</a>
                        <a href="watchlist" className="btn btn-outline-dark m-2 buttonStyle" target='_blank' rel="noreferrer">Add to Watchlist</a>
                    </div>
@@ -63,8 +65,8 @@ export const ProductList = () => {
                     </div> */}
                 {/* </div> */}
                 
-            </div>
-        </div>
+           </div>
+         </div>
     );
 }
 
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -18,38 +18,38 @@ export const QUERY_PRODUCTS = gql`
 
 
 export const QUERY_ALL_PRODUCTS = gql`
-  {
-    products {
-      _id
-      title
-      description
-      startBid
-      image
-      category 
-
-      
-    }
-  }
-`;
-
-//Working
-export const QUERY_CATEGORY = gql`
-  {
-    query categories ($category:String) {
-      categories(category:$category) {
+  
+     {
+      products{
         _id
         title
         description
-        startBid
-        category 
         image
+        startBid
+        category
       }
-         
     }
-    
-    
-  }
+  
 `;
+
+//Working
+// export const QUERY_CATEGORY = gql`
+//   {
+//     query categories ($category:String) {
+//       categories(category:$category) {
+//         _id
+//         title
+//         description
+//         startBid
+//         category 
+//         image
+//       }
+         
+//     }
+    
+    
+//   }
+// `;
 
 // Working
 export const QUERY_USER = gql`
