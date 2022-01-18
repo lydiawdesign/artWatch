56289af42e34ec5f405ab03c7661b7fb52b954e8
lydiawdesign <lydia.williamson.OSU@gmail.com>
2022-01-18 18:16:31 -0500

    deploying to heroku round 2

--- server/server.js
+++ server/server.js
@@ -25,8 +25,8 @@ app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
 // }
 
