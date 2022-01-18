65385827d22d853044465bddfaa7373112de1e11
Sheri Foster <fostr.sheri@gmail.com>
2022-01-17 19:58:17 -0500

    "sheris changes"

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,17 +1,36 @@
-import React from 'react'
+import React from 'react';
 // import '../App.css';
 import Azaman1 from '../../testImage/Azaman1.jpg';
+import { useQuery } from '@apollo/client';
+import { QUERY_ALL_PRODUCTS } from '../../utils/queries';
 
 
 
 export const ProductList = () => {
+    const {loading, data} = useQuery(QUERY_ALL_PRODUCTS);
+
+    const products = data?.products || []; 
+    console.log(products);
     return (
         <div className="container p-5">
             <h1 className='text-center pt-5'>View our Gallery:</h1>
 
            <div className="card-group">
 
-                <div className="card g-3 p-2">
+               {products.map(product => (
+                   <div className="card g-3 p-2">
+                   <img src={Azaman1} className="card-img-top" alt="Azaman"/>
+                   <div className="card-body card text-center">
+                       <h5 className="card-title">Azaman 1</h5>
+                       <p className="card-text">this is a short description for the art. </p>
+                       <p className="card-text"> start bid is $300</p>
+                       <a href="gallery" className="btn btn-outline-dark m-2 buttonStyle" target='_blank' rel="noreferrer">View Product</a>
+                       <a href="watchlist" className="btn btn-outline-dark m-2 buttonStyle" target='_blank' rel="noreferrer">Add to Watchlist</a>
+                   </div>
+               </div>
+               ) )}
+
+                {/* <div className="card g-3 p-2">
                     <img src={Azaman1} className="card-img-top" alt="Azaman"/>
                     <div className="card-body card text-center">
                         <h5 className="card-title">Azaman 1</h5>
@@ -20,9 +39,9 @@ export const ProductList = () => {
                         <a href="gallery" className="btn btn-outline-dark m-2 buttonStyle" target='_blank' rel="noreferrer">View Product</a>
                         <a href="watchlist" className="btn btn-outline-dark m-2 buttonStyle" target='_blank' rel="noreferrer">Add to Watchlist</a>
                     </div>
-                </div>
+                </div> */}
 
-                <div className="card g-3 p-2">
+                {/* <div className="card g-3 p-2">
                     <img src={Azaman1} className="card-img-top" alt="Azaman"/>
                     <div className="card-body card text-center">
                         <h5 className="card-title">Azaman 1</h5>
@@ -31,9 +50,9 @@ export const ProductList = () => {
                         <a href="gallery" className="btn btn-outline-dark m-2 buttonStyle" target='_blank' rel="noreferrer">View Product</a>
                         <a href="watchlist" className="btn btn-outline-dark m-2 buttonStyle" target='_blank' rel="noreferrer">Add to Watchlist</a>
                     </div>
-                </div>
+                </div> */}
 
-                <div className="card g-3 p-2">
+                {/* <div className="card g-3 p-2">
                     <img src={Azaman1} className="card-img-top" alt="Azaman"/>
                         <div className="card-body card text-center">
                         <h5 className="card-title">Azaman 1</h5>
@@ -41,8 +60,8 @@ export const ProductList = () => {
                         <p className="card-text"> start bid is $300</p>
                         <a href="gallery" className="btn btn-outline-dark m-2 buttonStyle" target='_blank' rel="noreferrer">View Product</a>
                         <a href="watchlist" className="btn btn-outline-dark m-2 buttonStyle" target='_blank' rel="noreferrer">Add to Watchlist</a>
-                    </div>
-                </div>
+                    </div> */}
+                {/* </div> */}
                 
             </div>
         </div>
